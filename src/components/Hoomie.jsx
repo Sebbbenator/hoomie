c30b2df321540ee0663ec69879fed3a3f2dd68c3
danielbilkvist <danielbilkvist@gmail.com>
2026-05-29 11:57:33 +0200

    Added Eyes and Files

---
+++ src/components/Hoomie.jsx
@@ -0,0 +1,12 @@
+import "../css/Hoomie.css";
+
+export default function Hoomie() {
+  return (
+    <div className="hoomie">
+      <h1 className="hoomie__title">Welcome to Hoomie!</h1>
+      <p className="hoomie__subtitle">
+        Your ultimate roommate management app. Sign up or log in to get started!
+      </p>
+    </div>
+  );
+}
